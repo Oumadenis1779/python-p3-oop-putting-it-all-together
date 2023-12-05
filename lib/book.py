86e920c4733fbb71102f5c6c63ab865bec1a64db
Oumadenis1779 <dennies.ouma@student.moringaschool.com>
2023-12-05 09:05:48 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__ (self,title,page_count):
+        self.title=title
+        self._page_count=272
+        self.current_page = 1
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count=property(get_page_count,set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     pass
+
+soldiers=Book("we are the soldiers",272)
+print(soldiers.title)
+print(soldiers.page_count)
+soldiers.page_count=300
+print(soldiers.page_count)
+print(soldiers.turn_page)
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self,brand,size):
+        self.brand=brand
+        self._size=size
+        self.condition = 'New'
+
+    def get_size(self):
+        return self._size
+        
+    def set_size(self,size):
+        if isinstance(size,int):
+            self._size=size
+        else:
+            print("size must be an integer")
+    
+    def cobble(self):
+        self.condition = 'New'
+        print("Your shoe is as good as new!")
+        return("The shoe has been repaired.")
+
+    size=property(get_size,set_size,)
     pass
+Prada=Shoe("Prada",9)
+print(Prada.size)
+print(Prada.brand)
+print(Prada.cobble())
